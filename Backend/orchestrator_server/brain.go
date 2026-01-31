ed42f5012f36e0d7838f7ab7152bab50333b77f2
Kritagya123611 <jhakritagya45@gmail.com>
2026-02-01 00:05:06 +0530

    fixed a bug in the regex detection

---
+++ Backend/orchestrator_server/brain.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+)
+
+//this file would listens for alerts and will eventually talk to the AI.
+
+func main(){
+	fmt.Println("Aegis Orchestrator Brain is online and listening for alerts...")
+}
